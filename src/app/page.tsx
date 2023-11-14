e5fc212c11f4614426d80dcc4b4e687d643c9011
Sergiyan92 <sergiyan@ukr.net>
2023-11-14 11:25:49 +0200

    create image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import type { Metadata } from "next";
-
+import Image from "next/image";
+import img from "../../public/img.jpg";
 export const metadata: Metadata = {
   title: "Home Page",
   description: "",
@@ -10,6 +11,10 @@ export default function Home() {
   return (
     <div>
       <Header name={"Serhii"} age={30} isMan={true}>
+        <div className="w-[500px]">
+          <Image src={img} alt="img" />
+        </div>
+
         <p>A message from Home</p>
       </Header>
     </div>
