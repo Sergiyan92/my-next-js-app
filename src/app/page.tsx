3566c12dac06927f935fc196029a939fd089366d
Sergiyan92 <sergiyan@ukr.net>
2023-11-14 14:32:01 +0200

    add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,52 @@
-"use client";
-// import Header from "@/components/Header";
+// "use client";
+import Header from "@/components/Header";
 import type { Metadata } from "next";
-// import Image from "next/image";
-// import img from "../../public/img.jpg";
-import { useState } from "react";
-// export const metadata: Metadata = {
-//   title: "Home Page",
-//   description: "",
-// };
+import Image from "next/image";
+import img from "../../public/img.jpg";
+// import { useState } from "react";
+export const metadata: Metadata = {
+  title: "Home Page",
+  description: "",
+};
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const fetchDataFromApi = async () => {
-    try {
-      setLoading(true);
-      const response = await fetch("/api/users", {
-        headers: {
-          Accept: "application/json",
-          method: "GET",
-        },
-      });
-      if (response) {
-        const data = await response.json();
-        console.log(data);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const [loading, setLoading] = useState(false);
+  // const fetchDataFromApi = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const response = await fetch("/api/users", {
+  //       headers: {
+  //         Accept: "application/json",
+  //         method: "GET",
+  //       },
+  //     });
+  //     if (response) {
+  //       const data = await response.json();
+  //       console.log(data);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
   return (
     <div>
-      <button
+      {/* <button
         className="px-4 py-2 font-bold text-white bg-blue-500"
         onClick={() => fetchDataFromApi()}
       >
         {loading ? "Loading..." : "Call my API"}
-      </button>
-      {/* <Header name={"Serhii"} age={30} isMan={true}>
+      </button> */}
+      <Header name={"Serhii"} age={30} isMan={true}>
         <div className="w-[500px]">
           <Image src={img} alt="img" />
         </div>
 
         <p>A message from Home</p>
-      </Header> */}
+      </Header>
+      <h1>Hello kitti</h1>
+      <h2>Congrats World</h2>
     </div>
   );
 }
