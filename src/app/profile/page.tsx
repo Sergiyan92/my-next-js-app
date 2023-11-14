7f0ada9e27e4f48c4cb5dbe678850f048706275d
Sergiyan92 <sergiyan@ukr.net>
2023-11-14 09:56:27 +0200

    create Link

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 const ProfilePage = () => {
   const router = useRouter();
@@ -7,11 +8,16 @@ const ProfilePage = () => {
     <div>
       <h1>My list of Profile</h1>
       <div>
-        <ul>
-          <li onClick={() => router.push("/profile/1")}>Profile 1</li>
-          <li onClick={() => router.push("/profile/2")}>Profile 2</li>
-          <li onClick={() => router.push("/profile/3")}>Profile 3</li>
-        </ul>
+        <Link href="/profile/1">Profile 1</Link>
+        <Link href="/profile/2">Profile 2</Link>
+        <Link href="/profile/3">Profile 3</Link>
+        <a
+          target="_blank"
+          rel="noreferrer noopener"
+          href="https://github.com/Sergiyan92"
+        >
+          GitHub
+        </a>
       </div>
     </div>
   );
