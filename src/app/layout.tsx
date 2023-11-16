364b7287a942fb963a996ececb6cd056056d5a7e
Sergiyan92 <sergiyan@ukr.net>
2023-11-16 11:34:06 +0200

    add context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster, toast } from "sonner";
 import "./globals.css";
+import { AppWrapper } from "@/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
